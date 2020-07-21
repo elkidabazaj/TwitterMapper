57066ec4a0fb4da80fd94a1a46832f3fc99435e2
Elkida Bazaj <bazajelkida@gmail.com>
2020-07-21 21:32:59 +0200

    Task 4 - modification of Query class

--- src/query/Query.java
+++ src/query/Query.java
@@ -1,17 +1,23 @@
 package query;
 
 import filters.Filter;
+import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.Layer;
+import twitter4j.Status;
+import ui.MapMarkerSimple;
+import util.Util;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * A query over the twitter stream.
  * TODO: Task 4: you are to complete this class.
  */
-public class Query {
+public class Query implements Observer {
     // The map on which to display markers when the query matches
     private final JMapViewer map;
     // Each query has its own "layer" so they can be turned on and off all at once
@@ -67,7 +73,16 @@ public class Query {
      * TODO: Implement this method
      */
     public void terminate() {
+        layer.setVisible(false);
+    }
 
+    @Override
+    public void update(Observable observable, Object obj) {
+        Status status = (Status) obj;
+        if (filter.matches(status)) {
+            Coordinate coordinate = Util.statusCoordinate(status);
+            map.addMapMarker(new MapMarkerSimple(layer, coordinate));
+        }
     }
 }
 
